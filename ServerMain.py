d55d9fdab0265d07d66860d34dec47f6946d6fd8
kai_Ker <kai_Ker@buaa.edu.cn>
2024-02-12 00:00:01 +0800

    [Feat] The test router "/" done

--- ServerMain.py
+++ ServerMain.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
 import uvicorn
+
+from routers.connection import router as router_conn
 
 app = FastAPI(docs_url=None)
 
-
-@app.get("/index", response_class=HTMLResponse)
-def Initial():
-    return "<h1>Hello, world!</h1>"
+app.include_router(router_conn)
 
 
 if __name__ == "__main__":
-    # 浏览器输入地址：
-    # http://localhost:8080/index 可以访问 /index 路由
     uvicorn.run(app, host="0.0.0.0", port=8080)
---
+++ routers/connection.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/")
+def MainPage():
+    """
+    测试连接，前端的 设置 界面中需要使用
+    :return: 一个内容为 "Success!" 的字符串
+    """
+    return "Success!"
