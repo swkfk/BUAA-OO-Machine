77ea9157c095200841b8ff4be1ff522d62ca7dbf
kai_Ker <kai_Ker@buaa.edu.cn>
2024-03-18 21:13:40 +0800

    [Feat] Remove the dependency of PyQt for exception hook

--- WinMain.py
+++ WinMain.py
@@ -1,19 +1,25 @@
 import sys
 import traceback as tb
+from tkinter import messagebox
 
-from PyQt6.QtWidgets import QApplication, QMessageBox
-
-from src.ui.MainWidget import MainWidget
-
+dependency = ["PyQt6", "QDarkStyle", "requests", "websocket-client（04xxxx 版本增加）"]
 
 def exception_hook(exctype, value, traceback):
+    if exctype == ModuleNotFoundError:
+        messagebox.showerror("模块未安装！", "请使用 pip 安装下列模块\n" + "\n".join(dependency))
+        sys.exit(1)
     s = "\n".join(tb.format_exception(exctype, value, traceback))
-    QMessageBox.critical(None, "Exception detected!", s)
+    messagebox.showerror("Exception detected!", s)
     sys.exit(1)
 
 
+sys.excepthook = exception_hook
+
+
 if __name__ == "__main__":
+    from PyQt6.QtWidgets import QApplication
+    from src.ui.MainWidget import MainWidget
+
     app = QApplication(sys.argv)
-    sys.excepthook = exception_hook
     widget = MainWidget()
     sys.exit(app.exec())
