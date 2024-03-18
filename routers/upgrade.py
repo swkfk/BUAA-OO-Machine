882d9f55d42d88f2fc0c9fac73ee8f8baa72f0dc
kai_Ker <kai_Ker@buaa.edu.cn>
2024-03-18 22:46:51 +0800

    [Bugfix] Fix the wrong new-line in the version

--- routers/upgrade.py
+++ routers/upgrade.py
@@ -10,4 +10,4 @@ def AskVersion():
     version_file = DB_ROOT / "version"
     if not version_file.exists():
         return "0"
-    return version_file.read_text()
+    return version_file.read_text().strip()
